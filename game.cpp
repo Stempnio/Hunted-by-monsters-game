e9eb65ee11e61bd88372f692eaee2408a8f7e014
Stempnio <masterkubajba@gmail.com>
2018-03-29 13:12:58 +0200

    0.03
    movement

--- game.cpp
+++ game.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <windows.h>
+#include <conio.h>
 
 using namespace std;
 void type_good();
 void draw(char field[][30]);
 void menu(char field[][30]);
 void basic_field(char field[][30]);
+void movement(char field[][30]);
+void end_check();
 void settings();
 
 
 int main()
 {
     char field[20][30];
+    int player_position[20][30];
+    int p_x;
+    int p_y;
+
     menu(field);
-    draw(field);
+    //draw(field);
+    //while()
 
 }
 
 void menu(char field[][30])
 {
-
-int choice;
+    system("cls");
+    int choice;
     for(int i=0; i<30; i++)
         cout << '$';
     cout << endl;
@@ -33,14 +41,15 @@ int choice;
     for(int i=0; i<30; i++)
         cout << '$';
     cout << endl << endl << "TYPE OPTION (1-3): ";
-    cin >> choice;
+    choice = getch();
     switch(choice)
     {
-        case 1: draw(field); break;
-        //case 2: settings(); break;
-        case 3:
+        case 49: basic_field(field); break;
+        //case 50: settings(); break;
+        case 51:
                 cout << "Goodbye!";
                 exit(0);
+                break;
         default:
                 cout << "WRONG OPTION, PLEASE ENTER AGAIN" << endl;
                 break;
@@ -48,9 +57,21 @@ int choice;
 }
 void basic_field(char field[][30])
 {
+    system("cls");
+
     for(int i=0; i<20; i++)
         for(int j=0; j<30; j++)
             field[i][j]='.';
+
+    field[18][15] = 'P';
+
+    for(int i=0; i<20; i++)
+    {
+        cout << endl;
+        for(int j=0; j<30; j++)
+            cout << field[i][j];
+    }
+
 }
 void draw(char field[][30])
 {
@@ -62,9 +83,18 @@ void draw(char field[][30])
         for(int j=0; j<30; j++)
             cout << field[i][j];
     }
-
-    for(int i=0; i<20; i++)
-        for(int j=0; j<30; j++)
-            field[i][j]='.';
 }
 
+void movement(char field[][30])
+{
+    int wsad;
+    wsad = getch();
+
+    switch(wsad)
+    {
+    case 119:
+        field
+        break;
+    case
+    }
+}
