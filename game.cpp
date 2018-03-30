8ee2d31c332570a7d931886c3b0f9f77d36bdf6a
Stempnio <masterkubajba@gmail.com>
2018-03-30 19:24:26 +0200

    0.11
    comeback from tutorial not working

--- game.cpp
+++ game.cpp
@@ -13,8 +13,8 @@ void player_movement(char field[][30], int& p_x, int& p_y);
 void monster_movement(char field[][30], int m_x[100], int m_y[100], int& p_x, int& p_y,  int& monster_counter);
 void end_check(char field[][30], int& p_x, int& p_y, int m_x[100], int m_y[100],  int& monster_counter, bool& again);
 //void score();
-//void settings();
-void draw_position(int& p_x, int& p_y, int m_x[100], int m_y[100]);
+void tutorial(char field[][30]);
+void draw_position(int& p_x, int& p_y, int m_x[100], int m_y[100]); // do sprawdzania bledow
 void continuation(bool& again);
 
 int main()
@@ -41,12 +41,12 @@ int main()
         again = true;
         while(again == true)
         {
-        end_check(field, p_x, p_y, m_x, m_y, monster_counter, again);
-        //draw_position(p_x, p_y, m_x, m_y);
-        draw(field);
-        monster_movement(field, m_x, m_y, p_x, p_y, monster_counter);
-        player_movement(field, p_x, p_y);
-        draw(field);
+            //draw_position(p_x, p_y, m_x, m_y);
+            draw(field);
+            monster_movement(field, m_x, m_y, p_x, p_y, monster_counter);
+            player_movement(field, p_x, p_y);
+            draw(field);
+            end_check(field, p_x, p_y, m_x, m_y, monster_counter, again);
         }
     }
 
@@ -58,7 +58,8 @@ void menu(char field[][30])
     int choice;
     for(int i=0; i<30; i++)
         cout << '$';
-    cout << endl;
+
+    cout << endl << endl;
     cout << "       START GAME - [1]" << endl;
     cout << endl;
     cout << "       SETTINGS - [2]" << endl;
@@ -78,8 +79,9 @@ void menu(char field[][30])
             basic_field(field);
             exit_loop = true;
             break;
-
-        //case 50: settings(); break;
+        case 50:
+            tutorial(field);
+            break;
         case 51:
                 cout << "GOODBYE!";
                 exit(0);
@@ -101,17 +103,30 @@ void basic_field(char field[][30])
 void draw(char field[][30])
 {
     system("cls");
+    cout << "   ";
+    for(int i=0; i<30; i++)
+        cout << '$';
+
+    cout << endl;
 
     for(int i=0; i<20; i++)
     {
         cout << endl;
+        cout << "$   ";
         for(int j=0; j<30; j++)
-            cout << field[i][j];
+                cout << field[i][j];
+
+        cout << "  $";
     }
 
+    cout << endl << endl;
+    cout << "   ";
+
+    for(int i=0; i<30; i++)
+        cout << '$';
 }
 
-void draw_position(int& p_x, int& p_y, int m_x[100], int m_y[100])
+void draw_position(int& p_x, int& p_y, int m_x[100], int m_y[100]) // do sprawdzania bledow
 {
     cout << endl << "p x " << p_x << " p y " << p_y << "m x " << m_x[0] << " m y " << m_y[0] << endl;
     Sleep(1000);
@@ -127,33 +142,33 @@ void player_movement(char field[][30], int& p_x, int& p_y)
     case 119: // w
         if(p_y>0)
         {
-        field[p_y][p_x] = '.';
-        p_y--;
-        field[p_y][p_x]= 'P';
+            field[p_y][p_x] = '.';
+            p_y--;
+            field[p_y][p_x]= 'P';
         }
         break;
     case 115: // s
         if(p_y<17) // 19 ale zanika przez m
         {
-        field[p_y][p_x] = '.';
-        p_y++;
-        field[p_y][p_x]= 'P';
+            field[p_y][p_x] = '.';
+            p_y++;
+            field[p_y][p_x]= 'P';
         }
         break;
     case 97: // a
         if(p_x>0)
         {
-        field[p_y][p_x] = '.';
-        p_x--;
-        field[p_y][p_x]= 'P';
+            field[p_y][p_x] = '.';
+            p_x--;
+            field[p_y][p_x]= 'P';
         }
         break;
     case 100: // d
         if(p_x<29)
         {
-        field[p_y][p_x] = '.';
-        p_x++;
-        field[p_y][p_x]= 'P';
+            field[p_y][p_x] = '.';
+            p_x++;
+            field[p_y][p_x]= 'P';
         }
         break;
     default:
@@ -203,8 +218,8 @@ void end_check(char field[][30], int& p_x, int& p_y, int m_x[100], int m_y[100],
     {
         if( (m_y[i] == p_y && m_x[i] == p_x-1) || (m_y[i] == p_y && m_x[i] == p_x+1) || (m_y[i] == p_y+1 && m_x[i] == p_x) || (m_y[i] == p_y-1 && m_x[i] == p_x))
         {
-            cout << endl << "ZOSTALES ZLAPANY PRZEZ MONSTERA" << endl;
-            Sleep(3000);
+            cout << endl << endl << "ZOSTALES ZLAPANY PRZEZ MONSTERA" << endl;
+            Sleep(1000);
             continuation(again);
         }
     }
@@ -214,21 +229,57 @@ void continuation(bool& again)
 {
     int choice;
     cout << endl << "PLAY AGAIN [1]" << endl << "EXIT GAME [2]" << endl;
+    while(again == true)
+    {
     choice = getch();
 
-    switch(choice)
-    {
-    case 49:
-        again = false;
-        break;
-    case 50:
-        cout << "GOODBYE!";
-        exit(0);
-        break;
-    default:
-        cout << "WRONG OPTION, PLEASE ENTER AGAIN" << endl;
-        break;
+        switch(choice)
+        {
+        case 49:
+            again = false;
+            break;
+        case 50:
+            cout << "GOODBYE!";
+            exit(0);
+            break;
+        default:
+            cout << "WRONG OPTION, PLEASE ENTER AGAIN" << endl;
+            break;
+        }
     }
 }
 
+void tutorial(char field[][30])
+{
+    system("cls");
+    for(int i=0; i<30; i++)
+        cout << '$';
 
+    cout << endl << endl;
+    cout << "   IN THIS GAME YOU HAVE TO RUNAWAY FROM MONSTERS (M)" << endl;
+    cout << "   TO MOVE PRESS W,S,A,D" << endl;
+    cout << "   YOU CAN AVOID MONSTERS BY GOING ONTO THEM IN PROPER WAY" << endl;
+    cout << "   GO BACK TO MENU [1]" << endl << endl;
+
+    for(int i=0; i<30; i++)
+        cout << '$';
+
+    cout << endl << "TYPE OPTION: ";
+
+    int go_back;
+    bool exit_loop = false;
+    do
+    {
+        go_back = getch();
+        switch(go_back)
+        {
+            case 49:
+                menu(field);
+                exit_loop = true;
+                break;
+            default:
+                cout << "WRONG OPTION, PLEASE ENTER AGAIN" << endl;
+                break;
+        }
+    }while(exit_loop==false);
+}
