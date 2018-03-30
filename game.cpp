caad08e388de367c3f5232cdf4dcb7f5a7a1d7e1
Stempnio <masterkubajba@gmail.com>
2018-03-30 15:08:10 +0200

    0.08
    fixed crashing, when monter_counter > 40

--- game.cpp
+++ game.cpp
@@ -10,10 +10,11 @@ void draw(char field[][30]);
 void menu(char field[][30]);
 void basic_field(char field[][30]);
 void player_movement(char field[][30], int& p_x, int& p_y);
-void monster_movement(char field[][30], int m_x[100], int m_y[100], int& monster_counter);
-void end_check(char field[][30], int& p_x, int& p_y);
+void monster_movement(char field[][30], int m_x[100], int m_y[100], int& p_x, int& p_y,  int& monster_counter);
+void end_check(char field[][30], int& p_x, int& p_y, int m_x[100], int m_y[100],  int& monster_counter);
 void score();
 void settings();
+void draw_position(int& p_x, int& p_y, int m_x[100], int m_y[100]);
 
 int main()
 {
@@ -23,21 +24,24 @@ int main()
     int p_y;
     int m_x[100];
     int m_y[100];
-    for(int i=0; i<40; i++)
+    for(int i=0; i<100; i++)
     {
-    m_x[i] = rand()%29;
-    m_y[i] = 1;
+    m_x[i] = rand()%30;
+    m_y[i] = 0;
     }
     menu(field);
-    int monster_counter=0;
+    int monster_counter=1;
     p_x=15;
-    p_y=18;
+    p_y=17;
     while(1)
     {
+    //end_check(field, p_x, p_y, m_x, m_y, monster_counter);
+    //draw_position(p_x, p_y, m_x, m_y);
     draw(field);
-    monster_movement(field, m_x, m_y, monster_counter);
     player_movement(field, p_x, p_y);
-    end_check(field, p_x, p_y);
+    draw(field);
+    monster_movement(field, m_x, m_y, p_x, p_y, monster_counter);
+    draw(field);
     }
 
 }
@@ -98,6 +102,13 @@ void draw(char field[][30])
         for(int j=0; j<30; j++)
             cout << field[i][j];
     }
+
+}
+
+void draw_position(int& p_x, int& p_y, int m_x[100], int m_y[100])
+{
+    cout << endl << "p x " << p_x << " p y " << p_y << "m x " << m_x[0] << " m y " << m_y[0] << endl;
+    Sleep(1000);
 }
 
 void player_movement(char field[][30], int& p_x, int& p_y)
@@ -116,7 +127,7 @@ void player_movement(char field[][30], int& p_x, int& p_y)
         }
         break;
     case 115: // s
-        if(p_y<19)
+        if(p_y<17) // 19 ale zanika przez m
         {
         field[p_y][p_x] = '.';
         p_y++;
@@ -139,37 +150,57 @@ void player_movement(char field[][30], int& p_x, int& p_y)
         field[p_y][p_x]= 'P';
         }
         break;
+    default:
+        break;
     }
 }
 
-void monster_movement(char field[][30], int m_x[100], int m_y[100], int& monster_counter)
+void monster_movement(char field[][30], int m_x[100], int m_y[100], int& p_x, int& p_y, int& monster_counter)
 {
     if(monster_counter<100)
     {
-    for(int i=0; i<monster_counter+1; i++)
+    for(int i=0; i<monster_counter; i++)
     {
-        if(m_y[i]<20)
+        if(m_y[i]<19)
         {
-            field[m_y[i]][m_x[i]] = 'M';
-            field[m_y[i]-1][m_x[i]] = '.';
+            field[m_y[i]][m_x[i]] = '.';
             m_y[i]++;
+            field[m_y[i]][m_x[i]] = 'M';
         }
+        else
+            field[m_y[i]][m_x[i]] = '.';
+
     }
     monster_counter++;
     }
     else
-        exit(0);
+    {
+        for(int i=0; i<monster_counter; i++)
+        {
 
+            if(m_y[i]<19)
+            {
+                field[m_y[i]][m_x[i]] = 'M';
+                field[m_y[i]-1][m_x[i]] = '.';
+                m_y[i]++;
+            }
+            else
+                field[m_y[i]-1][m_x[i]] = '.';
+        }
+    }
 
 }
 
-void end_check(char field[][30], int& p_x, int& p_y)
+void end_check(char field[][30], int& p_x, int& p_y, int m_x[100], int m_y[100], int& monster_counter)
 {
-    if(field[p_x][p_y]=='M')
+    for(int i=0; i<monster_counter+1; i++)
     {
-        cout << endl;
-        cout << "przegrales lamusie" << endl;
-        exit(0);
+        if( (m_y[i] == p_y && m_x[i] == p_x-1) || (m_y[i] == p_y && m_x[i] == p_x+1) || (m_y[i] == p_y+1 && m_x[i] == p_x) || (m_y[i] == p_y-1 && m_x[i] == p_x) )
+        {
+            cout << "kuniec" << endl;
+            exit(0);
+        }
     }
 }
+
 
